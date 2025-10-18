0a5f1101a3c0bacb39fb45119cdcde210cf8f074
Rashedul Alam <rashed464@outlook.com>
2025-10-18 12:08:34 +0600

    code updated

--- obj/Debug/net9.0/LibraryApiMongo.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryApiMongo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApiMongo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c41eb6c808a12149721dc6acb4baa24d4f86af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87d556f622c76116bba868a880af306c72abc1fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApiMongo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApiMongo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
